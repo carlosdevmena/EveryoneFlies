b8cb75bf92662bc9906090d1cb14abda798ffb9f
Carlos Palacios <carlospalaciosmena9@gmail.com>
2024-10-11 11:53:42 -0500

    Initial commit

---
+++ src/test/java/com/EveryoneFlies/EveryoneFlies/EveryoneFliesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EveryoneFlies.EveryoneFlies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EveryoneFliesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
